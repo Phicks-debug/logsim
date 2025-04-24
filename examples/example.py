7b4705b25bac555ddcd679eee7157d20b55c0a1d
Phicks-debug <an.tq@techxcorp.com>
2025-04-24 15:21:10 +0700

    updated to logsim

---
+++ examples/example.py
@@ -0,0 +1,8 @@
+from logsim import CustomLogger
+
+log = CustomLogger()
+log.debug("This is a DEBUG message")
+log.info("This is an INFO message")
+log.warning("This is a WARNING message")
+log.error("This is an ERROR message")
+log.critical("This is a CRITICAL message")
